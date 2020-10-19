44151ee3d0ab2c01281b495c3420d2d32ca285e6
Akli Amrous <akamrous@gmail.com>
2020-10-18 22:21:50 -0700

    Display Results

--- apiquery.py
+++ apiquery.py
@@ -7,6 +7,7 @@ News API allowing for a one call request
 
 """
 from newsapi import NewsApiClient
+from datetime import datetime as dt
 
 class APIQuery(object):
 
@@ -18,7 +19,7 @@ class APIQuery(object):
 
     Class Methods:
     search(self, query, category):  -Query the API and return the results
-
+    parse(self, results):           -Parse the data
     """
     def __init__(self, api_key):
         self.api_key = api_key
@@ -28,9 +29,42 @@ class APIQuery(object):
     def search(self, query, category):
         # Make the call to the API with a specificed category and query
         # Return the dictionary of articles
-        return self.client.get_top_headlines(q=query, category=category)['articles']
+        print(category)
+        return self.client.get_top_headlines(q=query, category=category)
 
     
+    def parse(self, results):
+        # This is a function to parse the data
+        # Into a displayable format
+        sources = []
+        headlines = []
+        date_published = []
+        cover_images = []
+        links = []
+        descriptions = []
+        for article in results['articles']:
+            sources.append(article['source']['name'])
+            headlines.append(article['title'])
+            date_published.append(article['publishedAt'].split("T")[0])
+            cover_images.append(article['urlToImage'])
+            links.append(article['url'])
+            descriptions.append(article['description'])
+
+        parsed = {
+
+            'sources': sources,
+            'headlines': headlines,
+            'date_published': date_published,
+            'cover_images': cover_images,
+            'links': links,
+            'descriptions': descriptions
+
+        }
+
+        return parsed
+
+        
+        
 
     
 
--- app.py
+++ app.py
@@ -11,8 +11,11 @@ Copyright (c) Akli Amrous
 from flask import Flask, render_template, url_for, request, redirect
 from apiquery import APIQuery 
 
+
 news = APIQuery('6868852668184abea077fa547303028a')
 app = Flask(__name__)
+app.secret_key="akliamrous"
+
 
 @app.route('/', methods=["POST", "GET"])
 def home():
@@ -20,9 +23,11 @@ def home():
         try:
             query = request.form['query']  
             category = request.form['category']
+            print(category)
             if(query  == ""):
                 return redirect(url_for("failed"))
-            return redirect(url_for("results",query=query, category=category ))
+            
+            return redirect(url_for("results", query=query, category=category ))
             
             # The user must fill all fields or else the app cannot work
         except Exception as e: 
@@ -34,12 +39,15 @@ def home():
 
 @app.route("/results/<query>/<category>", methods=["GET"])
 def results(query, category):
-    results = news.search(query, category) # Perform the API call with the query info
-    if(results == []):
+    check = news.search(query, category)['articles']
+    results = news.parse(news.search(query, category))
+    length = len(results['sources'])
+    data = (results, length)
+    if(check == []):
 
         return redirect(url_for("notfound"))
     else:
-        return f"<h1> {results[0]['title']}</h1>"
+        return render_template("results.html", data=data)
 
 
 @app.route("/failed", methods=["GET"])
